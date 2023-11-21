14fddfee43eee0ad0c24fefea0b188f286b08555
Aghalya Narayanan <itsaghalya@gmail.com>
2023-11-20 17:33:13 -0800

    started final project

--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
 // navigation bar routing
 document.addEventListener('DOMContentLoaded', function() {
-    var navHome = document.getElementById('home');
+    var navHome = document.getElementById('search');
     navHome.onclick = function() {
-        window.location.href = '/home/homePage/home.html';
-    };
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var navTimeline = document.getElementById('timeline');
-    navTimeline.onclick = function() {
-        window.location.href = '/timeline/timelinePage/timeline.html';
-    };
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    var navAnalytics = document.getElementById('analytics');
-    navAnalytics.onclick = function() {
-        window.location.href = '/analytics/analyticsPage/analytics.html';
+        window.location.href = 'home.html';
     };
 });
